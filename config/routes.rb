490e3cd729c1f2954ad771b823b8d32d42cf9b91
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-04-28 19:56:00 +0200

    Fixed some bugs on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
   get 'about',    to: 'pages#about',     as: :about
   get 'contact',  to: 'pages#contact',   as: :contact
-
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
