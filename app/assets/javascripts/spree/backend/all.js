0cdc219b1a2ec06658908d41d8d77ff71d4edb67
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-06-23 15:35:42 +0200

    Added spree backend js again due to rejected deployment

---
+++ app/assets/javascripts/spree/backend/all.js
@@ -0,0 +1,9 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require rails-ujs
+//= require spree/backend
+//= require_tree .
