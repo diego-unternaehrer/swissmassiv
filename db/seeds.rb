b7415cd5269342ec8ad9373e6f4ca18366620c90
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-09-07 13:35:11 +0200

    Removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Partner.create(name: "Felder Sport AG", address: "Rothornstrasse 14A", zip_code: "6174", place: "Sörenberg", country: "Schweiz", link: "www.intersportfelder.ch")
-Partner.create(name: "Hofherr Skiverleih", address: "Berwang 46", zip_code: "6622", place: "Berwang", country: "Österreich", link: "www.sport-hofherr.at")
-Event.create(date: Date.new(2020,10,3), place: "Sörenberg (Rossweid)", country: "Schweiz", cancelled: false)
-Event.create(date: Date.new(2020,11,6), place: "Zermatt", country: "Schweiz", cancelled: false)
-Event.create(date: Date.new(2020,12,1), place: "Damüls", country: "Österreich", cancelled: false)
-
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
