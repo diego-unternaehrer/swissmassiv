1043bfb4e6363c5ccb91811fda3e75beb1d2377d
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2021-10-10 23:56:24 +0200

    SendGrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,14 +72,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    domain:               'swissmassiv.ch',
-    user_name:            'apikey',
-    password:             ENV['SENDGRID_API_KEY'],
-    authentication:       :plain,
-    enable_starttls_auto: true  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
