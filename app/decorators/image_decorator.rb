c5f14857a3f688b5e7ca2cddca598250993a6bb9
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2023-05-12 18:50:40 +0200

    added active record migration file

---
+++ app/decorators/image_decorator.rb
@@ -0,0 +1,3 @@
+Spree::Image.class_eval do
+  has_one_attached :as_attachment
+end
---
+++ lib/tasks/migrate_images_to_activestorage.rb
@@ -0,0 +1,33 @@
+namespace :active_storage do
+  desc "Migrate images to use Active Storage"
+  task migrate_images: :environment do
+    puts '*' * 50
+    puts "Start migrating #{Spree::Image.count} images..."
+
+    Spree::Image.find_each do |image|
+      next if !image.attachment.present? || image.as_attachment.attached?
+
+      picture = image.attachment
+      picture.cache_stored_file!
+      file = picture.sanitized_file.file
+      content_type = "image/png"
+      filename = picture.filename
+      image.as_attachment.attach(io: file, content_type: content_type, filename: filename)
+      image.attachment.remove!
+      image.save
+    end
+
+    puts "Completed migrating #{Spree::Image.count} profiles..."
+    puts '*' * 50
+  end
+  desc 'Rename as_attachment to attachment'
+  task rename_as_attachment_to_attachment: :environment do
+    sql = <<-SQL
+      UPDATE active_storage_attachments
+      SET name = 'attachment'
+      WHERE name = 'as_attachment';
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+end
